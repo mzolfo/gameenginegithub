8b70ef4f173d72b002b151d538c48f1d21d0e9e2
mzolfo <32077843+mzolfo@users.noreply.github.com>
2017-11-20 15:44:41 -0600

    finalsubmit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Coin : MonoBehaviour
+{
+
+    public static int coinCount;
+    public bool hasWon = false;
+    private Text coinCountText;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+
+    private void Start()
+    {
+       
+        coinCountText = GameObject.Find("coinCountText").GetComponent<Text>();
+        coinCountText.text = "Boxes: " + coinCount;
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Scene wherearewe = SceneManager.GetActiveScene();
+
+        if (wherearewe.name == "demoscene")
+        {
+            coinCount = 7;
+        }
+        else if (wherearewe.name == "lvtwo")
+        {
+            coinCount = 8;
+        }
+        else if (wherearewe.name == "lvthree")
+        {
+            coinCount = 10;
+        }
+    }
+
+    
+
+
+    private void Update()
+    {
+        if (Input.GetKey("escape"))
+        { Application.Quit(); }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Scene wherearewe = SceneManager.GetActiveScene();
+        if (collision.gameObject.tag == "Player")
+        {
+            playerMovement tobechecked = collision.gameObject.GetComponent<playerMovement>();
+            if (tobechecked.isDashing)
+            {
+                coinCount--;
+                coinCountText.text = "Boxes: " + coinCount;
+                spriteRenderer.enabled = false;
+                boxCollider2D.enabled = false;
+                if (coinCount <= 0)
+                {
+                    if (wherearewe.name == "demoscene")
+                    {
+                        coinCount = 8;
+                        SceneManager.LoadScene(1);
+                        
+                    }
+                    else if (wherearewe.name == "lvtwo")
+                    {
+                        coinCount = 10;
+                        SceneManager.LoadScene(2);
+                    }
+                    else if (wherearewe.name == "lvthree")
+                    {
+                        coinCountText.text = "VICTORY: \nPress Escape!!";
+                        hasWon = true;
+                    }
+                }
+            }
+        }
+    }
+}
+
+//using unityengine.scenemanager
+
+    //scenemanager.loadscene("name of scene")
+
+/*static int coinCount = 0;
+
+
+    private Text coinCountText;
+    private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+
+    private void Start()
+    {
+        coinCountText = GameObject.Find("CoinCountText").GetComponent<Text>();
+        coinCountText.text = "Coin Count: " + coinCount;
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            audioSource.Play();
+            coinCount++;
+            coinCountText.text = "Coin Count: " + coinCount;
+            spriteRenderer.enabled = false;
+            boxCollider2D.enabled = false;
+            //Destroy(gameObject);
+        }
+    }*/
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    [SerializeField]
+    float movementSpeed = 4;
+    [SerializeField]
+    float jumpForce = 300;
+    [SerializeField]
+    bool grounded = false;
+    [SerializeField]
+    float dashForce = 10;
+    [SerializeField]
+    float dashTime = 0.8f;
+    
+
+    
+   public bool isDashing = false;
+
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    Rigidbody2D myRigidbody;
+	// Use this for initialization
+	void Start () {
+        //this code teleports the gameobject to a new location
+        //transform.position = new Vector3(0, 0, 0);
+
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+	}
+
+   private IEnumerator ResetIsDashingAfterTime()
+    {
+        yield return new WaitForSeconds(dashTime);
+        myRigidbody.gravityScale = 1;
+        isDashing = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Move();
+        Jump();
+        DetectGround();
+        PlayerDash();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && grounded == true)
+        {
+            myRigidbody.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    void Move()
+    {
+        if (!isDashing)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            myRigidbody.velocity = new Vector2(horizontalInput * movementSpeed, myRigidbody.velocity.y);
+        }
+    }
+
+
+    void DetectGround()
+    {
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        /*  Collider2D[] groundObjects = Physics2D.OverlapCircleAll(
+            groundDetectPoint.position, groundDetectRadius, whatCountsAsGround);
+
+        isOnGround = groundObjects.Length > 0;*/
+    }
+
+    private void PlayerDash()
+    {
+        if (Input.GetButtonDown("Fire1") && !isDashing) 
+            //upon pressing space or rmb the player dashes in whatever direction they are facing
+            //if the player is not moving in either direction they do nothing
+        {
+            isDashing = true;
+
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                myRigidbody.velocity = new Vector2(dashForce, 0);
+                myRigidbody.gravityScale = 0;
+                StartCoroutine(ResetIsDashingAfterTime());
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                myRigidbody.velocity = new Vector2((dashForce * -1), 0);
+                myRigidbody.gravityScale = 0;
+                StartCoroutine(ResetIsDashingAfterTime());
+            }
+            else if (Input.GetAxis("Horizontal") == 0)
+            {
+                isDashing = false;
+            }   
+        }
+    }
+
+  
+    //objective
+    //create a game in which the player can dash and strike enemies. if the player is not dashing they will
+    //"die" and be reset to their starting position upon contact with an enemy if the player is dashing the
+    //enemy object will be deleted
+
+    //player's collision handler
+   
+    
+    //how to destroy enemy objects
+    
+
+}
